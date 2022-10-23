9dd34d51735b42824e6019d70eeb5cf808cf07a6
velmuruganp2794 <velmuruganp2794@gmail.com>
2022-10-23 11:19:46 +0530

    Sliding window approach

--- src/main/java/SlidingWindow/LC_1984_MinimumDifference.java
+++ src/main/java/SlidingWindow/LC_1984_MinimumDifference.java
@@ -29,16 +29,31 @@ public class LC_1984_MinimumDifference {
 	1. Set minimum = Integer.MAX_VALUE
 	2. Traverse elements in array from 0 to end-1;
     3. Traverse in inner loop from j=k-1 
-    4. Find minimum between nums[j]-nums[i]
+    4. Find Math.min (minimum , nums[j]-nums[i])
     5. Re-assign minimum to nums[j]-nums[j], if minimum > nums[j]-nums[i]
     6. Return minimum;		
     */
+	
+	/*
+	Pseudo Code: Sliding Approach.
+	1. Set minimum = 0, pointer=0
+	2. Find difference for first window and set it as minimum nums[k-1] - nums[pointer++]
+	3. traverse element from j=k as maximum and pointer index as minimum
+	4. find the difference	and compare with minimum value
+	5. re-assign minimum as nums[j] - nums[pointer] if it is less than available minimum
+	6. return minimum
+    */
+	
+	/*
+	 * Time Complexity = O(n-k) or O(n)
+	 * Space Complexity = O(n) or O(1)
+	 */
 	
 	@Test
 	public void positive(){
 		
 		int[] array= {9,4,1,7};
-		int minimumDifference = minimumDifference(array, 4);
+		int minimumDifference = minimumDifference_Sliding(array, 2);
 		System.out.println(minimumDifference);
 	}
 		
@@ -54,6 +69,26 @@ public class LC_1984_MinimumDifference {
             	System.out.println("i="+i+", j="+j);
                 minimum = Math.min(minimum, nums[j]-nums[i]);
             }
+        }
+        return minimum;
+    }
+	
+	public int minimumDifference_Sliding(int[] nums, int k) { // 1 , 4, 7, 9
+        Arrays.sort(nums);
+        int minimum=0, pointer=0;
+
+        //Find minimum to first window
+        minimum = nums[k-1]-nums[pointer++];
+        
+        // Check for next window
+        System.out.println(minimum);
+        for(int j=k;j<nums.length;j++){
+        	System.out.println("j="+j+", pointer="+pointer);
+
+            minimum = Math.min(minimum,nums[j]-nums[pointer]);
+            System.out.println(minimum);
+            pointer++;
+
         }
         return minimum;
     }
